96994d0c0358e03a5389bf3c83e274a1a55040f2
Johan Groenen <johan.groenen@gmail.com>
2021-03-29 15:46:18 +0200

    add digital product lead

--- js/tags.js
+++ js/tags.js
@@ -6,6 +6,7 @@
         "onstopbare brainstormer": "#ff4000",
         "strategic design": "#9BC995",
         "strategisch ontwerper": "#9BC995",
+        "digital product lead": "#9BC995",
         "clean coding": "#00CFC1",
         "alles is inspiratie": "#000000",
         "co-creatie": "#000000"
