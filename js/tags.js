e425acf5f5892f64b33f49902463dd948af3c958
Johan Groenen <johan.groenen@gmail.com>
2021-05-04 00:23:59 +0200

    add archetype

--- js/tags.js
+++ js/tags.js
@@ -1,30 +1,60 @@
 (function () {
-    var tagColors = {
-        "open samenwerking": "#F0A202",
-        "communities of practice": "#ED254E",
-        "#publictech": "#266B96",
-        "onstopbare brainstormer": "#ff4000",
-        "strategic design": "#9BC995",
-        "strategisch ontwerper": "#9BC995",
-        "digital product lead": "#9BC995",
-        "clean coding": "#00CFC1",
-        "alles is inspiratie": "#000000",
-        "co-creatie": "#000000"
+    var archetypes = {
+        "creator": {
+            "naam": "maker (70%)",
+            "wil": "innovatie",
+            "toon": "inspirerend, gewaagd, aanstootgevend",
+            "strategie": [
+                "mensen inspireren hun verbeelding te gebruiken",
+                "aanmoedigen om originaliteit na te jagen"
+            ],
+            "kleur": "#d664be",
+            "tags": [
+                "software design",
+                "onstopbare brainstormer",
+                "strategic design",
+                "alles is inpiratie"
+            ]
+        },
+        "everyman": {
+            "naam": "gewone man (30%)",
+            "wil": "saamhorigheid",
+            "toon": "vriendelijk, bescheiden, authentiek",
+            "strategie": [
+                "het uitlijnen van waarden",
+                "het maken van een verwelkomende gemeenschap"
+            ],
+            "kleur": "#ff8600",
+            "tags": [
+                "open samenwerking",
+                "communities of practice",
+                "co-creatie",
+                "community",
+                "#publictech"
+            ]
+        }
     };
 
-    // UNDERLINE TAGS IN TEXT
-    var text = document.body.innerHTML;
-    for (let tag in tagColors) {
-        console.log("%c highlighting " + tag, 'background: ' + tagColors[tag] + '; color: #fff');
-        text = text.replace(tag, "<span style='border-bottom: 2px dashed " + tagColors[tag] + "'>" + tag + "</span>");
-    }
-    document.body.innerHTML = text;
+    // MESSAGE
+    for (let archetype in archetypes) {
+        let info = archetypes[archetype];
+        console.log(`%c${archetype}, ${info.naam}, wil ${info.wil} door ${info.strategie.join(" en ")}`, `background: ${info.kleur}; color: #fff`);
 
-    // COLOR TAGS
-    var tags = document.getElementsByClassName('tag');
-    for (let tag of tags) {
-        if (tagColors[tag.innerHTML.toLowerCase()]) {
-            tag.style.backgroundColor = tagColors[tag.innerHTML.toLowerCase()];
+        // UNDERLINE TAGS IN TEXT
+        var text = document.body.innerHTML;
+        for (let index in info.tags) {
+            let tag = info.tags[index];
+            console.log(`%c highlighting ${tag}`, `background: ${info.kleur}; color: #fff`);
+            text = text.replace(tag, `<span style='border-bottom: 2px dashed ${info.kleur}'>${tag}</span>`);
+        }
+        document.body.innerHTML = text;
+
+        // COLOR TAGS
+        var tags = document.getElementsByClassName('tag');
+        for (let tag of tags) {
+            if (info.tags.indexOf(tag.innerHTML.toLowerCase()) !== -1) {
+                tag.style.backgroundColor = info.kleur;
+            }
         }
     }
 })();
