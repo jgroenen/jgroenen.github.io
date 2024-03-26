59e3896811d9945a12f8d9f973c7df15be2d3944
Johan Groenen <johan@jgroenen.nl>
2024-03-26 14:52:29 +0100

    computer for easterEgg

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ var canvasElement = document.getElementsByTagName("canvas")[0];
 if (canvasElement) {
     var easterEgg = new EasterEgg(canvasElement, ['#000000', '#00FF00']);
     imageElement.addEventListener('click', function () {
+        document.getElementsByClassName("banner-image")[0].style.display = "none";
+        document.getElementById("easterEgg").style.display = "block";
         if (easterEgg.isRunning()) return;
         var ctx = canvasElement.getContext("2d");
         ctx.fillStyle = "#000000";
         ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
-        canvasElement.style.height = canvasElement.height + 'px';
         easterEgg.start();
     });
 }
